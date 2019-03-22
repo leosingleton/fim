468dec0ee7b7c789130a5a9c2ba88b62ca62b5b0
Leo Singleton <leo@leosingleton.com>
2019-03-22 16:54:06 +0530

    Using Jest for NodeJS testing; Karma/Jasmine for web
    Jest doesn't have support for running a true web browser like
    Chrome, and jsdom just isn't good enough for low-level canvas and
    WebGL operations.

---
+++ webpack.test.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+var glob = require('glob');
+
+var config = {
+  entry: glob.sync('./src/**/__tests__/*.ts'),
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.ts' ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build/test'),
+    filename: 'test.js',
+    library: 'library'
+  }
+};
+
+module.exports = config;
