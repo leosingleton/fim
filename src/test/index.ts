a2775c47294bb3938cf5853e8ccae7f4c73a328a
Leo Singleton <leo@leosingleton.com>
2019-03-25 19:42:28 -0400

    Missing change from previous commit

--- src/test/index.ts
+++ src/test/index.ts
@@ -2,5 +2,5 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
-export * from './FImTestImages';
+export * from './FimTestImages';
 export * from './FimTestPatterns';
