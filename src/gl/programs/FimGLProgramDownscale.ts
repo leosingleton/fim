1bffc9001930309e03cdd4359ad6c6c135d3d2c7
Leo Singleton <leo@leosingleton.com>
2019-08-25 21:48:37 -0400

    Work-in-progress
    Attempt at a generic algorithm, but it doesn't properly calculate
    weights.

--- src/gl/programs/FimGLProgramDownscale.ts
+++ src/gl/programs/FimGLProgramDownscale.ts
@@ -50,7 +50,7 @@ export class FimGLProgramDownscale extends FimGLProgram {
   private pixelCount: number;
 
   private calculateSamplePixels(xRatio: number, yRatio: number): void {
-    let xPixels = this.calculateSamplePixelsOneAxis(xRatio);
+    let xPixels = this.calculateSamplePixelsOneAxis(xRatio); console.log(`${xRatio} -> ${xPixels}`);
     let yPixels = this.calculateSamplePixelsOneAxis(yRatio);
 
     let xCount = xPixels.length;
@@ -76,6 +76,52 @@ export class FimGLProgramDownscale extends FimGLProgram {
    * @returns Array of [offset, weight] tuples for the pixels we should sample
    */
   private calculateSamplePixelsOneAxis(ratio: number): number[][] {
-    return [[-1, 0.5], [1, 0.5]];
+    // Ratio is conveniently the number of pixels we need to sample, centered on zero...
+    let halfRatio = ratio / 2;
+
+    // First, calculate the number of samples we must take. With linear filtering, we can get the weighted average of 4
+    // adjacent pixels with each texture2D call.
+    let count = Math.ceil(halfRatio);
+
+    // If there's only one pixel to sample, there's no offset or weight
+    if (count === 1) {
+      return [[0, 1]];
+    }
+
+    // Calculate the rightmost sample point. This follows a stairstep function...
+    //  ratio ==> count ==> rightmost
+    //  2.001 ==>   2   ==>  0.5
+    //  4.000 ==>   2   ==>  1
+    //  4.001 ==>   3   ==>  1.5
+    //  6.000 ==>   3   ==>  2
+    //  6.001 ==>   4   ==>  2.5
+    //  8.000 ==>   4   ==>  3
+    //  8.001 ==>   5   ==>  3.5
+    // 10.000 ==>   5   ==>  4
+    let rightMost = (halfRatio + count - 2) / 2;
+
+    let weight = 1 / count; // BUGBUG: The weight of the rightmost and leftmost points should not be equal!
+    let pixels = [];
+
+    if (count % 2 === 0) {
+      // Even number of samples
+      for (let n = 0; n < count / 2 - 1; n++) {
+        let offset = n * 2 + 1;
+        pixels.push([offset, weight]);
+        pixels.push([-offset, weight]);
+      }
+    } else {
+      // Odd number of samples
+      pixels.push([0, weight]);
+      for (let n = 0; n < Math.floor(count / 2) - 1; n++) {
+        let offset = (n + 1) * 2;
+        pixels.push([offset, weight]);
+        pixels.push([-offset, weight]);
+      }
+    }
+
+    pixels.push([rightMost, weight]);
+    pixels.push([-rightMost, weight]);
+    return pixels;
   }
 }
