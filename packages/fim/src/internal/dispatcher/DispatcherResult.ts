f1d1e4b9925da9e7a9205f8c0b7e8f2435446f3f
Leo Singleton <leo@leosingleton.com>
2020-02-07 00:43:25 -0500

    More implementation of the dispatcher/rendering engine

--- packages/fim/src/internal/dispatcher/DispatcherResult.ts
+++ packages/fim/src/internal/dispatcher/DispatcherResult.ts
@@ -2,7 +2,17 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
-/** Command results */
+/** Command results. Either commandResult or commandError will be sent depending on success or errror, respectively. */
 export interface DispatcherResult {
+  /** Sequence number provided in the command */
+  sequenceNumber: number;
 
+  /** Unique string identifying the FIM engine command */
+  command: string;
+
+  /** On success, contains the output of the command */
+  commandResult?: any;
+
+  /** On failure, contains the exception that caused the command to fail */
+  commandError?: Error;
 }
---
+++ packages/fim/src/internal/engine/direct/Engine.ts
@@ -0,0 +1,42 @@
+// FIM - Fast Image Manipulation Library for Javascript
+// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+// See LICENSE in the project root for license information.
+
+import { Dispatcher } from '../../dispatcher/Dispatcher';
+import { DispatcherCommand } from '../../dispatcher/DispatcherCommand';
+import { DispatcherResult } from '../../dispatcher/DispatcherResult';
+import { FimError, FimErrorCode } from '../../../primitives/FimError';
+
+/** Low-level FIM rendering engine */
+export abstract class Engine implements Dispatcher {
+  public dispatchCommand(command: DispatcherCommand): void {
+    // Populate required fields of any result
+    const resultObject: DispatcherResult = {
+      sequenceNumber: command.sequenceNumber,
+      command: command.command
+    };
+
+    try {
+      // Attempt to execute the command
+      const result = this.executeCommand(command);
+
+      // Return the successful result
+      resultObject.commandResult = result;
+      this.onCommandResult(resultObject);
+    } catch (err) {
+      // Return the error
+      resultObject.commandError = err;
+      this.onCommandResult(resultObject);
+    }
+  }
+
+  public onCommandResult: (result: DispatcherResult) => void;
+
+  /** Derived classes should overload this message to handle any commands they add to FIM */
+  protected executeCommand(command: DispatcherCommand): any {
+    switch (command.command) {
+      default:
+        throw new FimError(FimErrorCode.AppError, `Unknown command ${command.command}`);
+    }
+  }
+}
