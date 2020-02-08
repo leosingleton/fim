fdf90495dd89c7c0cdb7a8920f494f58af42b6cd
Leo Singleton <leo@leosingleton.com>
2020-02-08 10:35:22 -0500

    Fixing Karma to run unit tests from the fim-browser package
    I had to work around a ts-loader issue to get this to work.

---
+++ packages/fim-browser/src/internal/engine/direct/__tests__/BrowserEngine.browser.ts
@@ -0,0 +1,26 @@
+// FIM - Fast Image Manipulation Library for Javascript
+// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
+// See LICENSE in the project root for license information.
+
+/*
+import { BrowserEngine } from '../BrowserEngine';
+import { FimReleaseResourcesFlags } from '@leosingleton/fim';
+
+describe('BrowserEngine', () => {
+
+  it('Creates, releases, and disposes', () => {
+    const eng = new BrowserEngine();
+    eng.releaseResources(FimReleaseResourcesFlags.All);
+    eng.dispose();
+  });
+
+});
+*/
+
+describe('Dummy test case', () =>{
+
+  it('Succeeds', () => {
+    expect(true).toBeTruthy();
+  });
+
+});
--- webpack.karma.js
+++ webpack.karma.js
@@ -8,8 +8,16 @@ const config = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          // ts-loader has an issue where it reads the tsconfig.json file for the first .ts file it encounters and
+          // doesn't look for different tsconfig.json files for other files. This causes an issue for FIM as each NPM
+          // package has a different rootDir, so unit tests from more than one package will fail to compile. To work
+          // around this, we force ts-loader to use the shared configuration file at the root of the repo.
+          // See https://github.com/TypeStrong/ts-loader/issues/647
+          configFile: path.resolve(__dirname, 'tsconfig.json')
+        }
       }
     ]
   },
