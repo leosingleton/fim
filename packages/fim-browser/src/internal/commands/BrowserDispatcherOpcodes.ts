2b9f9e449b923b636b19a30843087120b2cec555
Leo Singleton <leo@leosingleton.com>
2020-02-24 12:27:47 -0500

    Removing queue-related code
    The previous effort was leading to a lot of boilerplate code to
    queue and dequeue commands, all for a (possible) future remoting
    to a web worker and lookahead optimizations. And two weeks in, very
    little functionality was ported from v1.
    
    Going back to a simpler implementation for now...

--- packages/fim-browser/src/internal/commands/BrowserDispatcherOpcodes.ts
+++
@@ -1,9 +0,0 @@
-// FIM - Fast Image Manipulation Library for Javascript
-// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
-// See LICENSE in the project root for license information.
-
-/** String values used in the DispatcherCommand.opcode property specific to the web browser implementation */
-export enum BrowserDispatcherOpcodes {
-  Create = 'BrowserCreate',
-  CreateImage = 'BrowserCreateImage'
-}
--- packages/fim-node/src/internal/commands/NodeDispatcherOpcodes.ts
+++
@@ -1,9 +0,0 @@
-// FIM - Fast Image Manipulation Library for Javascript
-// Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
-// See LICENSE in the project root for license information.
-
-/** String values used in the DispatcherCommand.opcode property specific to the Node.js implementation */
-export enum NodeDispatcherOpcodes {
-  Create = 'NodeCreate',
-  CreateImage = 'NodeCreateImage'
-}
