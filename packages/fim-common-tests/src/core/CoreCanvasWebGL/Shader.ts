fc8d45bb02e57eac7cab56d6003f828b26059a32
Leo Singleton <leo@leosingleton.com>
2020-03-12 13:33:45 +0530

    Adding unit test for shaders with constants

--- packages/fim-common-tests/src/core/CoreCanvasWebGL/Shader.ts
+++ packages/fim-common-tests/src/core/CoreCanvasWebGL/Shader.ts
@@ -2,14 +2,17 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
-import { green, midpoint, red, small } from '../../common/Globals';
+import { green, grey, midpoint, red, small } from '../../common/Globals';
 import { using } from '@leosingleton/commonlibs';
 import { FimDimensions } from '@leosingleton/fim';
 import { CoreCanvasWebGL, CoreShader } from '@leosingleton/fim/internals';
 import { GlslShader } from 'webpack-glsl-minify';
 
-/** Sample WebGL shader to fill with a solid color */
-const testFillShader = require('../../glsl/FillUniform.glsl.js') as GlslShader;
+/** Sample WebGL shader to fill with a solid color specified by a constant */
+const testFillConstShader = require('../../glsl/FillConst.glsl.js') as GlslShader;
+
+/** Sample WebGL shader to fill with a solid color specified by a uniform */
+const testFillUniformShader = require('../../glsl/FillUniform.glsl.js') as GlslShader;
 
 /** CoreCanvasWebGL test cases for shaders */
 export function coreCanvasWebGLTestSuiteShader(
@@ -20,7 +23,7 @@ export function coreCanvasWebGLTestSuiteShader(
 
     it('Creates and disposes', () => {
       using(factory(small), canvas => {
-        const shader = canvas.createCoreShader(testFillShader);
+        const shader = canvas.createCoreShader(testFillConstShader);
         shader.dispose();
       });
     });
@@ -29,7 +32,7 @@ export function coreCanvasWebGLTestSuiteShader(
       let shader: CoreShader;
 
       using(factory(small), canvas => {
-        shader = canvas.createCoreShader(testFillShader);
+        shader = canvas.createCoreShader(testFillConstShader);
       });
 
       // Since the parent canvas was disposed, dispose() on the child object will throw an exception
@@ -38,7 +41,7 @@ export function coreCanvasWebGLTestSuiteShader(
 
     it('Executes a simple fill shader', () => {
       using(factory(small), canvas => {
-        const shader = canvas.createCoreShader(testFillShader);
+        const shader = canvas.createCoreShader(testFillUniformShader);
 
         // Fill the output with the color red
         shader.setUniform('uColor', [1, 0, 0, 1]);
@@ -49,9 +52,26 @@ export function coreCanvasWebGLTestSuiteShader(
       });
     });
 
-    it('Executes a simple fill shader, outputting to a texture', () => {
+    it('Executes a simple fill shader with a constant, outputting to a texture', () => {
       using(factory(small), canvas => {
-        const shader = canvas.createCoreShader(testFillShader);
+        const shader = canvas.createCoreShader(testFillConstShader);
+        const texture = canvas.createCoreTexture();
+
+        // Fill the texture with the color green
+        shader.setConstant('cColor', 0.5);
+        shader.execute(texture);
+
+        // Render the texture to the WebGL canvas
+        canvas.copyFrom(texture);
+
+        // Ensure the output is grey
+        expect(canvas.getPixel(midpoint(small)).distance(grey)).toBeLessThan(0.05);
+      });
+    });
+
+    it('Executes a simple fill shader with a uniform, outputting to a texture', () => {
+      using(factory(small), canvas => {
+        const shader = canvas.createCoreShader(testFillUniformShader);
         const texture = canvas.createCoreTexture();
 
         // Fill the texture with the color green
