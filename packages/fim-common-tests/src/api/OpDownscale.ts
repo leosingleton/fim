68cadd23abbceeb13309fee3825b88b8f43ea6ac
Leo Singleton <leo@leosingleton.com>
2020-03-16 13:10:31 -0400

    Cleaning up downscale unit test
    Also temporarily disabling 48x as it had some issues with too many
    uniforms in Chrome.

--- packages/fim-common-tests/src/api/OpDownscale.ts
+++ packages/fim-common-tests/src/api/OpDownscale.ts
@@ -2,7 +2,7 @@
 // Copyright (c) Leo C. Singleton IV <leo@leosingleton.com>
 // See LICENSE in the project root for license information.
 
-import { large } from '../common/Globals';
+import { grey, medium } from '../common/Globals';
 import { TestPatterns } from '../common/TestPatterns';
 import { usingAsync } from '@leosingleton/commonlibs';
 import { Fim, FimDimensions, FimOpDownscale, FimTextureSampling } from '@leosingleton/fim';
@@ -16,9 +16,9 @@ export function fimTestSuiteOpDownscale(
     it('Downscales at 4x', async () => testDownscale(factory, 4));
     it('Downscales at 8x', async () => testDownscale(factory, 8));
     it('Downscales at 16x', async () => testDownscale(factory, 16));
-    //it('Downscales at 24x', async () => testDownscale(factory, 24));
+    it('Downscales at 24x', async () => testDownscale(factory, 24, 0.1));
     it('Downscales at 32x', async () => testDownscale(factory, 32));
-    //it('Downscales at 48x', async () => testDownscale(factory, 48));
+//    it('Downscales at 48x', async () => testDownscale(factory, 48, 0.2));
 //    it('Downscales at 64x', async () => testDownscale(factory, 64));
     //it('Downscales at 96x', async () => testDownscale(factory, 96));
 //    it('Downscales at 128x', async () => testDownscale(factory, 128));
@@ -27,11 +27,12 @@ export function fimTestSuiteOpDownscale(
 
 async function testDownscale(
   factory: (maxImageDimensions: FimDimensions) => Fim,
-  ratio: number
+  ratio: number,
+  maxError = 0.05
 ): Promise<void> {
-  await usingAsync(factory(large), async fim => {
-    // Draw the test pattern on a 512x256 image
-    const inputDimensions = FimDimensions.fromWidthHeight(512, 256);
+  await usingAsync(factory(medium), async fim => {
+    // Draw the test pattern on a 256x512 image
+    const inputDimensions = FimDimensions.fromWidthHeight(256, 512);
     const input = fim.createImage(inputDimensions);
     input.imageOptions.sampling = FimTextureSampling.Linear;
     await TestPatterns.renderAsync(input, TestPatterns.downscaleStress);
@@ -45,14 +46,6 @@ async function testDownscale(
 
     // Sample a pixel in the center. It should be 50% grey
     const color = await output.getPixelAsync(outputDimensions.getCenter());
-    const min = 127 - 3;
-    const max = 127 + 3;
-    expect(color.r).toBeGreaterThan(min);
-    expect(color.r).toBeLessThan(max);
-    expect(color.g).toBeGreaterThan(min);
-    expect(color.g).toBeLessThan(max);
-    expect(color.b).toBeGreaterThan(min);
-    expect(color.b).toBeLessThan(max);
-    expect(color.a).toEqual(255);
+    expect(color.distance(grey)).toBeLessThan(maxError);
   });
 }
