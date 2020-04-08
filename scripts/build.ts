63648e5c087202086c8098b6cb15647c94afc35f
Leo Singleton <leo@leosingleton.com>
2020-04-08 09:29:03 -0400

    Refactoring build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -7,7 +7,6 @@
 import path from 'path';
 import cp from 'child_process';
 import glob from 'glob';
-//import webpack from 'webpack';
 
 /** NPM packages, in build order */
 const packages = [
@@ -21,23 +20,45 @@ const packagesDir = path.resolve(__dirname, '../packages');
 
 for (const pkg of packages) {
   try {
-    const workingDir = path.resolve(packagesDir, pkg);
-    console.log(workingDir);
+    const cwd = path.resolve(packagesDir, pkg);
+    console.log(cwd);
     let stdout = '';
 
     // Find and minify .glsl files
-    const srcDir = path.resolve(workingDir, 'src');
-    const files = glob.sync('**/*.glsl', { cwd: srcDir });
-    for (const file of files) {
-      stdout += cp.execSync(`npx webpack-glsl-minify ${file} -o ../build --stripVersion`, { cwd: srcDir });
-    }
+    stdout += minifyGlsl(cwd);
 
     // Compile TypeScript
-    stdout += cp.execSync('npx tsc', { cwd: workingDir });
+    stdout += compileCommonJS(cwd);
 
-    console.log(stdout.toString());
+    console.log(stdout);
   } catch (err) {
-    console.log(err.stdout.toString());
+    console.log(err.stdout);
     process.exit(-1);
   }
+}
+
+/**
+ * Minifies all .glsl files with the webpack-glsl-minfy compiler
+ * @param cwd Current working directory
+ * @return Contents of stdout
+ */
+function minifyGlsl(cwd: string): string {
+  let stdout = '';
+
+  const srcDir = path.resolve(cwd, 'src');
+  const files = glob.sync('**/*.glsl', { cwd: srcDir });
+  for (const file of files) {
+    stdout += cp.execSync(`npx webpack-glsl-minify ${file} -o ../build --stripVersion`, { cwd: srcDir });
+  }
+
+  return stdout;
+}
+
+/**
+ * Compiles a package using tsc and generates CommonJS output
+ * @param cwd Current working directory
+ * @return Contents of stdout
+ */
+function compileCommonJS(cwd: string): string {
+  return cp.execSync('npx tsc', { cwd }).toString();
 }
